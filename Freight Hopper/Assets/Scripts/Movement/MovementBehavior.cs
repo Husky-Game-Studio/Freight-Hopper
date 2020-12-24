1e4fdec4f82cac25e074bae24190af09286c3db6
NikhilAlapati <alapatinikhil@gmail.com>
2020-12-23 21:19:44 -0800

    Added Speed variable

--- Freight Hopper/Assets/Scripts/Movement/MovementBehavior.cs
+++ Freight Hopper/Assets/Scripts/Movement/MovementBehavior.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 public class MovementBehavior : MonoBehaviour {
     private UserInput input;
     private Transform transform;
+    [SerializeField] private float speed;
 
     private void Awake() {
+        speed = 10;
         input = GetComponent<UserInput>();
         transform = GetComponent<Transform>();
     }
 
     private void FixedUpdate() {
-        transform.Translate(new Vector3(input.Move().x, 0, input.Move().y));
+        transform.Translate(new Vector3(input.Move().x, 0, input.Move().y) * Time.deltaTime * speed);
     }
 }
