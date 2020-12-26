a3319d290e23523969865790b90a049a70a4ffe9
NikhilAlapati <alapatinikhil@gmail.com>
2020-12-26 10:20:47 -0800

    Player now moves relative to camera
    Fixed bug

--- Freight Hopper/Assets/Scripts/Movement/MovementBehavior.cs
+++ Freight Hopper/Assets/Scripts/Movement/MovementBehavior.cs
@@ -3,15 +3,22 @@ using UnityEngine;
 public class MovementBehavior : MonoBehaviour {
     private UserInput input;
     private Transform transform;
+    private Camera camera;
+    private Transform cameraTransform;
     [SerializeField] private float speed;
 
     private void Awake() {
         speed = 10;
         input = GetComponent<UserInput>();
         transform = GetComponent<Transform>();
+        camera = Camera.main;
+        cameraTransform = camera.transform;
     }
 
     private void FixedUpdate() {
-        transform.Translate(new Vector3(input.Move().x, 0, input.Move().y) * Time.deltaTime * speed);
+        Vector3 move = new Vector3(input.Move().x, 0f, input.Move().y);
+        move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
+        move.y = 0f;
+        transform.Translate(move * (Time.deltaTime * speed));
     }
 }
