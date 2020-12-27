ff004d3221e2619d2875ad95b6ba047b42f64918
NikhilAlapati <alapatinikhil@gmail.com>
2020-12-27 10:46:34 -0800

    added jump ability
    added jump ability but still there is a bug thta the jump button doenst always work

--- Freight Hopper/Assets/Scripts/Movement/JumpBehavior.cs
+++ Freight Hopper/Assets/Scripts/Movement/JumpBehavior.cs
@@ -1,4 +1,29 @@
+using System;
 using UnityEngine;
 
 public class JumpBehavior : MonoBehaviour {
+    private UserInput input;
+    private Transform transform;
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    private void Awake() {
+        input = GetComponent<UserInput>();
+        transform = GetComponent<Transform>();
+        isGrounded = true;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(UnityEngine.Collision other) {
+        if (other.gameObject.tag == "landable") {
+            isGrounded = true;
+        }
+    }
+
+    private void FixedUpdate() {
+        if (input.Jumped() && isGrounded) {
+            rb.AddForce(new Vector3(0f, 5f, 0f), ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
 }
