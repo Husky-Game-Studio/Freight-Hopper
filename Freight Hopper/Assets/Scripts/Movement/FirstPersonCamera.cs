904816541708eb4ca24b8c9d855794df80909ec0
Ayrton <ayrtonmuniz2002@gmail.com>
2020-12-30 00:52:28 -0800

    Camera Tilting API
    FirstPersonCamera now has a camera tilting functionality. 1 will tilt the camera to the right, 0 will put the camera to the default state of no tilt and -1 will tilt the camera to the left.

--- Freight Hopper/Assets/Scripts/Movement/FirstPersonCamera.cs
+++ Freight Hopper/Assets/Scripts/Movement/FirstPersonCamera.cs
@@ -10,14 +10,15 @@ public class FirstPersonCamera : MonoBehaviour
     private CinemachineVirtualCamera cam;
 
     [SerializeField] private CameraEffect fov;
-
-    private CameraEffect speedEffectsWeight;
+    [SerializeField] private CameraEffect tilt;
 
     [System.Serializable]
     private struct CameraEffect
     {
-        public float baseValue;
-        public float value;
+        [HideInInspector] public float baseValue;
+        [HideInInspector] public float value;
+        [HideInInspector] public float addedValue;
+
         public float maxValue;
         public float transitionSmoothness;
     }
@@ -30,16 +31,65 @@ public class FirstPersonCamera : MonoBehaviour
         fov.baseValue = cam.m_Lens.FieldOfView;
         fov.value = fov.baseValue;
 
+        tilt.baseValue = cam.m_Lens.Dutch;
+        tilt.value = tilt.baseValue;
+
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void FixedUpdate()
     {
-        float addedFov = playerRB.velocity.magnitude - playerMovement.Speed;
-        fov.value = Mathf.Lerp(fov.value, fov.baseValue + addedFov, fov.transitionSmoothness);
+        fov.addedValue = playerRB.velocity.magnitude - playerMovement.Speed;
+        fov.value = Mathf.Lerp(fov.value, fov.baseValue + fov.addedValue, fov.transitionSmoothness);
         fov.value = Mathf.Clamp(fov.value, fov.baseValue, fov.maxValue);
 
         cam.m_Lens.FieldOfView = fov.value;
+    }
+
+    /// <summary>
+    /// Expecting a value of -1 for left, 0 for none, 1 for right
+    /// </summary>
+    /// <param name="direction"></param>
+    public void Tilt(int direction)
+    {
+        StartCoroutine(Tilting(direction));
+    }
+
+    private IEnumerator Tilting(int direction)
+    {
+        float tiltSign = Mathf.Sign(tilt.value);
+        while (tilt.value != tilt.maxValue * direction)
+        {
+            if (direction != 0)
+            {
+                tilt.addedValue += 0.03f * direction;
+            }
+            else
+            {
+                tilt.addedValue += 0.03f * -tiltSign;
+            }
+
+            tilt.value = Mathf.Lerp(tilt.value, tilt.baseValue + tilt.addedValue, tilt.transitionSmoothness);
+
+            if (direction != 0)
+            {
+                tilt.value = Mathf.Clamp(tilt.value, -tilt.maxValue, tilt.maxValue);
+            }
+            else
+            {
+                if (tiltSign == 1)
+                {
+                    tilt.value = Mathf.Clamp(tilt.value, 0, tilt.maxValue);
+                }
+                else
+                {
+                    tilt.value = Mathf.Clamp(tilt.value, -tilt.maxValue, 0);
+                }
+            }
+
+            cam.m_Lens.Dutch = tilt.value;
+            yield return null;
+        }
     }
 }
