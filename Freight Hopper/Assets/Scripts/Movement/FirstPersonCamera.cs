746c835212f9f30b96577acc14562ef12ea67f65
Ayrton <ayrtonmuniz2002@gmail.com>
2020-12-29 17:13:43 -0800

    Camera FOV
    FOV maybe changes I am not sure

--- Freight Hopper/Assets/Scripts/Movement/FirstPersonCamera.cs
+++ Freight Hopper/Assets/Scripts/Movement/FirstPersonCamera.cs
@@ -1,19 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class FirstPersonCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody playerRB;
+    private CinemachineVirtualCamera cam;
+    [SerializeField] private CameraEffect fov;
+    private CameraEffect speedEffectsWeight;
+
+    [System.Serializable]
+    private struct CameraEffect
     {
+        public float baseValue;
+        public float scalingConstant;
+        public float maxValue;
+    }
+
+    private void Awake()
+    {
+        playerRB = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
+        cam = GetComponent<CinemachineVirtualCamera>();
+        fov.baseValue = cam.m_Lens.FieldOfView;
+
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        cam.m_Lens.FieldOfView = Mathf.Clamp((Mathf.Pow(playerRB.velocity.magnitude / 4 * fov.scalingConstant, 2)) * 60, fov.baseValue, fov.maxValue);
     }
 }
