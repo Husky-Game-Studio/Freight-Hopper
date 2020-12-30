66ea8d8a477c6e3f243940f6fa0703bdb1512f43
Ayrton <ayrtonmuniz2002@gmail.com>
2020-12-29 18:22:54 -0800

    xSpeedLimit and zSpeedLimit
    Speed limit needs to be broken up

--- Freight Hopper/Assets/Scripts/Movement/MovementBehavior.cs
+++ Freight Hopper/Assets/Scripts/Movement/MovementBehavior.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-public class MovementBehavior : MonoBehaviour {
+public class MovementBehavior : MonoBehaviour
+{
     private UserInput input;
     private Transform transform;
     private Rigidbody rb;
@@ -10,15 +11,19 @@ public class MovementBehavior : MonoBehaviour {
     [SerializeField] private float speed = 20f;
     [SerializeField] private float playerMoveSpeedLimit;
 
-    private void Awake() {
+    private void Awake()
+    {
         input = GetComponent<UserInput>();
         transform = GetComponent<Transform>();
         cameraTransform = Camera.main.transform;
         rb = GetComponent<Rigidbody>();
     }
 
-    private void Update() {
+    private void LateUpdate()
+    {
         moveDirection = new Vector3(input.Move().x, 0f, input.Move().y);
+        Debug.DrawRay(this.transform.position, moveDirection, Color.red);
+
         Vector3 cameraForward = cameraTransform.forward;
         Vector3 cameraRight = cameraTransform.right;
         cameraForward.y = 0f;
@@ -30,31 +35,44 @@ public class MovementBehavior : MonoBehaviour {
         relativeMove.y = 0f;
         Move(relativeMove);
 
-        if (relativeMove != Vector3.zero) {
+        if (relativeMove != Vector3.zero)
+        {
             gameObject.transform.forward = relativeMove;
         }
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
     }
 
-    private void Move(Vector3 direction) {
+    private void Move(Vector3 direction)
+    {
         direction.Normalize();
+        float theta = Mathf.Atan(direction.y / direction.x);
+        Debug.Log("Degrees: " + Mathf.Rad2Deg * theta);
+        float xSpeedLimit = playerMoveSpeedLimit * Mathf.Cos(theta);
+        float zSpeedLimit = playerMoveSpeedLimit * Mathf.Sin(theta);
         Vector3 velocity = direction * speed;
-        if (Mathf.Abs(rb.velocity.x) < playerMoveSpeedLimit) {
+
+        Debug.DrawRay(this.transform.position, velocity, Color.blue);
+        if (Mathf.Abs(rb.velocity.x) < xSpeedLimit)
+        {
             rb.velocity += new Vector3(velocity.x, 0f, 0f);
 
-            rb.velocity = new Vector3(Mathf.Clamp(rb.velocity.x, -playerMoveSpeedLimit, playerMoveSpeedLimit), rb.velocity.y, rb.velocity.z);
+            rb.velocity = new Vector3(Mathf.Clamp(rb.velocity.x, -xSpeedLimit, xSpeedLimit), rb.velocity.y, rb.velocity.z);
 
-            if (direction.x == 0) {
+            if (direction.x == 0)
+            {
                 rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
             }
         }
-        if (Mathf.Abs(rb.velocity.z) < playerMoveSpeedLimit) {
+        if (Mathf.Abs(rb.velocity.z) < zSpeedLimit)
+        {
             rb.velocity += new Vector3(0, 0, velocity.z);
-            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, Mathf.Clamp(rb.velocity.z, -playerMoveSpeedLimit, playerMoveSpeedLimit));
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, Mathf.Clamp(rb.velocity.z, -zSpeedLimit, zSpeedLimit));
 
-            if (direction.z == 0) {
+            if (direction.z == 0)
+            {
                 rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 0);
             }
         }
