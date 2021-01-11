d6b5829a4e8b0cb1e375fc3cd38a6a723c2be27c
Ayrton <ayrtonmuniz2002@gmail.com>
2021-01-11 15:46:06 -0800

    Wind functionality
    The wind does push the player as intended, however the movement is a bit bugged right now. No matter what, if the player is below a speed limit they won't move. The particles for this funcitonality still has to be made.

---
+++ Freight Hopper/Assets/Scripts/Enviroment/Wind.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    [SerializeField] private float strength = 1;
+    [SerializeField] private List<Rigidbody> effectedBodies = new List<Rigidbody>();
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * strength);
+    }
+
+    private void Awake()
+    {
+        // This is not a good way to find the player, but idk how to find the player since its in another scene.
+        // At least not yet
+    }
+
+    private void Start()
+    {
+        effectedBodies.Add(UserInput.Input.gameObject.GetComponent<Rigidbody>());
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (Rigidbody rb in effectedBodies)
+        {
+            rb.AddForce(transform.TransformDirection(Vector3.forward) * strength, ForceMode.Force);
+        }
+    }
+}
