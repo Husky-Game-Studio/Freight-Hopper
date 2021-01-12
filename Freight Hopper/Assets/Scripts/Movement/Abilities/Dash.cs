563ecde9044b2942ab2013cfe7d9c1cab53b5296
Ayrton <ayrtonmuniz2002@gmail.com>
2021-01-12 01:54:21 -0800

    Dash Implemented
    As before, movement bug causes this to have issues. It works, but needs to be adjusted of course and tweaked. Basic implementation as always.

---
+++ Freight Hopper/Assets/Scripts/Movement/Abilities/Dash.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour
+{
+    private bool dashPossible = true;
+    [SerializeField] private float dashForce;
+    [SerializeField] private Timer delay = new Timer(0.5f);
+
+    [Range(-90, 90)]
+    [SerializeField] private float angle; // with 0 being level with the ground
+
+    private bool dash = false;
+    private Rigidbody rb;
+
+    private void OnDrawGizmosSelected()
+    {
+        float radians = angle * Mathf.Deg2Rad;
+        Vector3 direction = new Vector3(0, Mathf.Sin(radians), Mathf.Cos(radians));
+        UnityEditor.Handles.color = Color.blue;
+        Gizmos.DrawRay(transform.position, transform.TransformDirection(direction));
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        delay.CountDown();
+        if (UserInput.Input.Dash() && !delay.TimerActive() && dashPossible)
+        {
+            dash = true;
+            dashPossible = false;
+            delay.ResetTimer();
+        }
+
+        if (GetComponent<JumpBehavior>().IsGrounded)
+        {
+            dashPossible = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (dash)
+        {
+            float radians = angle * Mathf.Deg2Rad;
+            Vector3 direction = new Vector3(0, Mathf.Sin(radians), Mathf.Cos(radians));
+            rb.AddForce(transform.TransformDirection(direction) * dashForce, ForceMode.Impulse);
+            dash = false;
+            dashPossible = false;
+        }
+    }
+}
