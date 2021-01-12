5ead103b36723dec6f2802ccaf568f1dfc4359cc
Ayrton <ayrtonmuniz2002@gmail.com>
2021-01-11 23:39:36 -0800

    Fixed Exit Portal Velocity
    Movement is still bugged tho

--- Freight Hopper/Assets/Scripts/Enviroment/Portal.cs
+++ Freight Hopper/Assets/Scripts/Enviroment/Portal.cs
@@ -21,7 +21,8 @@ public class Portal : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.transform.position = otherPortal.transform.position + otherPortal.transform.TransformDirection(exitPosition);
-            Vector3 reflectionNormal = (this.transform.forward + otherPortal.transform.forward) * 0.5f;
+            Vector3 reflectionNormal = (this.transform.forward + otherPortal.transform.forward);
+            reflectionNormal.Normalize();
             Debug.DrawRay((this.transform.position - otherPortal.transform.position), reflectionNormal, Color.red);
             Vector3 newVelocity = Vector3.Reflect(collision.attachedRigidbody.velocity, reflectionNormal);
             collision.attachedRigidbody.velocity = newVelocity;
