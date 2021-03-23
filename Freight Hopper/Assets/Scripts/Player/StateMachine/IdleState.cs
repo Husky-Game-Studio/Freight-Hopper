83984a6c7d58ce13faf119ae3ff1aa5c4f1babff
Gabriel O <olivergabe@hotmail.com>
2021-03-23 13:20:57 -0700

    IdleState X RunState interaction complete

--- Freight Hopper/Assets/Scripts/Player/StateMachine/IdleState.cs
+++ Freight Hopper/Assets/Scripts/Player/StateMachine/IdleState.cs
@@ -6,11 +6,20 @@ public class IdleState : BasicState
 {
     public BasicState DoState(PlayerMachineCenter myPlayer) {
         // if the run buttons was pressed, then go to run state (return myPlayer.runState)
+        if (myPlayer.userInput.Move() != Vector2.zero)
+        {
+            //Debug.Log("Should be in Run state!");
+            return myPlayer.runState;
+        }
+        else {
+            //Debug.Log("In Idle state");
+            return this;
+        }
 
         // if the jump button was pressed, then go to jump state (return myPlayer.jumpState)
 
         // if the <INSERT ABILITY> button was pressed, then go to <INSERT ABILITY> state (return myPlayer.<INSERT ABILITY>State)
         
-        return this;
+
     }
 }
--- Freight Hopper/Assets/Scripts/Player/StateMachine/RunState.cs
+++ Freight Hopper/Assets/Scripts/Player/StateMachine/RunState.cs
@@ -6,7 +6,16 @@ public class RunState : BasicState
 {
     public BasicState DoState(PlayerMachineCenter myPlayer)
     {
-        return this;
+        if (myPlayer.userInput.Move() != Vector2.zero)
+        {
+            //Debug.Log("in Run state!");
+            return this;
+        }
+        else
+        {
+            //Debug.Log("Should be in Idle state");
+            return myPlayer.idleState;
+        }
     }
 
 }
