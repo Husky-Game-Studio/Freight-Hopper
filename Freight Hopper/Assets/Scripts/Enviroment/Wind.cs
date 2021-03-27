902a92981d38145b0f3c6104cd85486bdca45cef
Ayrton <ayrtonmuniz2002@gmail.com>
2021-03-26 20:15:15 -0700

    Wind Using Raycasts
    Wind has started, its mostly done but there are some issues with performance and it doesn't work with portals yet.

--- Freight Hopper/Assets/Scripts/Enviroment/Wind.cs
+++ Freight Hopper/Assets/Scripts/Enviroment/Wind.cs
@@ -4,31 +4,80 @@ using UnityEngine;
 
 public class Wind : MonoBehaviour
 {
-    [SerializeField] private float strength = 1;
-    [SerializeField] private List<Rigidbody> effectedBodies = new List<Rigidbody>();
+    [SerializeField] private float forcePerPosition = 1;
+    [SerializeField] private int updatesPerSecond = 5;
+    [SerializeField] private Vector3 size;
+
+    [SerializeField, Range(0.4f, 0.9f)] private float width = 1;
+    [SerializeField] private Dictionary<Rigidbody, List<Vector3>> affectedBodies = new Dictionary<Rigidbody, List<Vector3>>();
+    [SerializeField] private LayerMask layers;
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * strength);
+        GizmosExtensions.DrawGizmosArrow(this.transform.position, this.transform.forward);
+
+        for (float x = -size.x / 2; x <= size.x / 2; x += width)
+        {
+            for (float y = -size.y / 2; y <= size.y / 2; y += width)
+            {
+                Vector3 position = transform.TransformPoint(new Vector3(x, y, 0));
+                Gizmos.DrawRay(position, transform.TransformDirection(Vector3.forward) * size.z);
+            }
+        }
     }
 
     private void Awake()
     {
-        // This is not a good way to find the player, but idk how to find the player since its in another scene.
-        // At least not yet
+        StartCoroutine(WindLoop(updatesPerSecond));
     }
 
-    private void Start()
+    private IEnumerator WindLoop(float frequency)
     {
-        effectedBodies.Add(UserInput.Input.gameObject.GetComponent<Rigidbody>());
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(1 / (float)frequency);
+            FindRigidbodies();
+        }
+    }
+
+    private void FindRigidbodies()
+    {
+        foreach (Rigidbody rb in affectedBodies.Keys)
+        {
+            affectedBodies[rb].Clear();
+        }
+
+        RaycastHit hit;
+        for (float x = -size.x / 2; x <= size.x / 2; x += width)
+        {
+            for (float y = -size.y / 2; y <= size.y / 2; y += width)
+            {
+                Vector3 position = transform.TransformPoint(new Vector3(x, y, 0));
+
+                if (Physics.Raycast(position, transform.forward, out hit, size.z, layers))
+                {
+                    if (hit.collider.attachedRigidbody != null)
+                    {
+                        if (!affectedBodies.ContainsKey(hit.collider.attachedRigidbody))
+                        {
+                            affectedBodies.Add(hit.collider.attachedRigidbody, new List<Vector3>());
+                        }
+                        affectedBodies[hit.collider.attachedRigidbody].Add(hit.point);
+                    }
+                }
+            }
+        }
     }
 
     private void FixedUpdate()
     {
-        foreach (Rigidbody rb in effectedBodies)
+        foreach (Rigidbody rb in affectedBodies.Keys)
         {
-            rb.AddForce(transform.TransformDirection(Vector3.forward) * strength, ForceMode.Force);
+            foreach (Vector3 position in affectedBodies[rb])
+            {
+                rb.AddForceAtPosition(transform.forward * forcePerPosition, position, ForceMode.Force);
+            }
         }
     }
 }
