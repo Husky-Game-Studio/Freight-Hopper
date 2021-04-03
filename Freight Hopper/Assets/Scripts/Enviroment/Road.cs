ced11dbc78a49ead2673a75910039511a59cc6a9
Flootow <43686541+Flootow@users.noreply.github.com>
2021-04-03 02:43:21 -0700

    Road Mesh Rotation Math
    Focused too long on fixing this small section of math. It is in a operational condition now, but doesn't implement roll

--- Freight Hopper/Assets/Scripts/Enviroment/Road.cs
+++ Freight Hopper/Assets/Scripts/Enviroment/Road.cs
@@ -67,7 +67,7 @@ public class Road
                 }
                 Vector3 currentPoint = roadPoints[i];
                 Vector3 direciton = GetPathDirection(i);
-                SegmentVertices(currentPoint, direciton).CopyTo(vers, pointsPerSegment * i);
+                SegmentVertices(currentPoint, direciton, cummulativeDistance / 6.0f).CopyTo(vers, pointsPerSegment * i);
                 SegmentUVs(cummulativeDistance).CopyTo(uvs, pointsPerSegment * i);
             }
             for (int i = 0; i < detail * path.NumSegments; i++)
@@ -110,23 +110,55 @@ public class Road
             return (roadPoints[i] - roadPoints[i - 1]).normalized;
     }
 
-    private Vector3[] SegmentVertices(Vector3 position, Vector3 direction)
+    private Vector3[] SegmentVertices(Vector3 position, Vector3 direction, float cummulativeDistance)
     {
         Vector3[] vertices = new Vector3[segmentPoints.Length];
         for(int i = 0; i < segmentPoints.Length; i++)
         {
-            vertices[i] = position + RotateVector(segmentPoints[i], direction);
+            vertices[i] = position + RotateVector(segmentPoints[i], direction, cummulativeDistance);
+            //vertices[i] = position + RotateAboutAxis(segmentPoints[i], Quaternion.LookRotation(Vector3.up) * direction, Vector3.ProjectOnPlane(direction, ; //Mathf.Deg2Rad * Quaternion.LookRotation(direction).eulerAngles.z
         }
         return vertices;
     }
 
     //TODO Create a version that rotates with respect to an "up axis"
+    /*
     private Vector3 RotateVector(Vector3 displacement, Vector3 direction)
     {
-        Vector3 projection = Vector3.ProjectOnPlane(displacement, Quaternion.LookRotation(direction) * Vector3.up);
+        Vector3 projection = Vector3.ProjectOnPlane(displacement, Quaternion.LookRotation(direction) * Quaternion.LookRotation(Vector3.up) * Vector3.forward);
         Vector3 projectionDisplacement = displacement - projection;
         Vector3 newProjection = Quaternion.LookRotation(projection) * Quaternion.LookRotation(direction) * Vector3.forward * projection.magnitude;
+        projectionDisplacement = Quaternion.LookRotation(projectionDisplacement) * Quaternion.LookRotation(direction) * Vector3.forward * projectionDisplacement.magnitude;
         return newProjection + projectionDisplacement;
+    }
+    private Vector3 RotateAboutAxis(Vector3 displacement, Vector3 axis, float theta)
+    {
+        Vector2 projCoord = Quaternion.Inverse(Quaternion.LookRotation(axis)) * Vector3.ProjectOnPlane(displacement, axis);
+        float projDisplacment = (displacement - Vector3.ProjectOnPlane(displacement, axis)).magnitude;
+        float newTheta = Mathf.Atan2(projCoord.y, projCoord.x) + theta;
+        Vector2 newProjCoord = new Vector2(Mathf.Cos(newTheta), Mathf.Sin(newTheta)) * projCoord.magnitude;
+        return Quaternion.LookRotation(axis) * new Vector3(newProjCoord.x, newProjCoord.y, projDisplacment);
+    }
+    */
+    private Vector3 RotateVector(Vector3 displacement, Vector3 direction, float deltaRoll = 0.0f)
+    {
+        float xzDirMag = Mathf.Sqrt(Mathf.Pow(direction.x, 2) + Mathf.Pow(direction.z, 2));
+        float xyDisMag = Mathf.Sqrt(Mathf.Pow(displacement.x, 2) + Mathf.Pow(displacement.y, 2));
+        float xzDisMag = Mathf.Sqrt(Mathf.Pow(displacement.x, 2) + Mathf.Pow(displacement.z, 2));
+        float yzDisMag = Mathf.Sqrt(Mathf.Pow(displacement.y, 2) + Mathf.Pow(displacement.z, 2));
+
+        //float roll = Mathf.Atan2(displacement.y, displacement.x) + deltaRoll; //Incorrectly Implemented, breaks other rotations
+        //displacement = new Vector3(Mathf.Cos(roll) * xyDisMag, Mathf.Sin(roll) * xyDisMag, displacement.z); //Roll Rotation
+
+        float pitch = Mathf.Atan2(displacement.y, displacement.z) + Mathf.Atan2(direction.y, xzDirMag);
+        displacement = new Vector3(displacement.x, Mathf.Sin(pitch) * yzDisMag, Mathf.Cos(pitch) * yzDisMag); //Pitch Rotation
+        
+        float yaw = Mathf.Atan2(displacement.x, displacement.z) + Mathf.Atan2(direction.x, direction.z);
+        displacement = new Vector3(Mathf.Sin(yaw) * xzDisMag, displacement.y , Mathf.Cos(yaw) * xzDisMag); //Yaw Rotation
+
+        
+
+        return displacement;
     }
 
     private Vector2[] SegmentUVs(float cummulativeDistance)
