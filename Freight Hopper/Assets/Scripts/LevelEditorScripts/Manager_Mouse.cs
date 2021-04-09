8b1d53f6d4d2e4c3a34918f3ce1f9cbca4d82af9
lycheefuzz <ninapan118@hotmail.com>
2021-04-09 00:20:18 -0700

    Added UI to move the spawn point
    Purely for prototyping/testing only -- hopefully don't need by final product

---
+++ Freight Hopper/Assets/Scripts/LevelEditorScripts/Manager_Mouse.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.IO.Compression;
+
+//First off: I just want to say that if anyone ever sees this: I am sorry. 
+//This design is shameful. Don't even look at it.
+
+//I had a hard time trying to wrap my head around what to do within the timeframe I had. :( 
+//I am just so busy and needed to at least try and turn in something that works. Next time, I will do better. 
+
+public partial class Manager : MonoBehaviour
+{
+   
+}
