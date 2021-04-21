fb0d289bdaa79f9c07306f21910aeb95890b7baa
Gabriel O <olivergabe@hotmail.com>
2021-04-20 18:44:23 -0700

    commenting groundpoundbehavior

--- Freight Hopper/Assets/Scripts/Player/Movement/Abilities/GroundPoundBehavior.cs
+++ Freight Hopper/Assets/Scripts/Player/Movement/Abilities/GroundPoundBehavior.cs
@@ -32,16 +32,17 @@ public class GroundPoundBehavior : MonoBehaviour
         playerCollision.CollisionDataCollected -= GroundPounding;
     }
 
+    // I need to move all of this to the PFSM check
     public void TryGroundPound()
     {
         if (groundPoundPossible)
         {
-            if (rb.velocity.y > 0)
+            if (rb.velocity.y > 0) // this line
             {
                 rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
             }
-            rb.AddForce(-CustomGravity.GetUpAxis(rb.position) * initialBurstForce, ForceMode.VelocityChange);
-            groundPounding = true;
+            rb.AddForce(-CustomGravity.GetUpAxis(rb.position) * initialBurstForce, ForceMode.VelocityChange); // to this line, is the initial state
+            groundPounding = true; // we are currently ground pounding
             groundPoundPossible = false;
         }
     }
@@ -49,33 +50,33 @@ public class GroundPoundBehavior : MonoBehaviour
     public void GroundPounding()
     {
         // Checks if ground pound is possible
-        if (!UserInput.Input.GroundPoundTriggered())
+        if (!UserInput.Input.GroundPoundTriggered()) // if the user is not holding the input, stop ground pounding ~~ let go of input
         {
             groundPoundPossible = true;
             groundPounding = false;
         }
         Vector3 upAxis = CustomGravity.GetUpAxis(rb.position);
-        if (groundPounding && playerCollision.ContactNormal.current != upAxis)
+        if (groundPounding && playerCollision.ContactNormal.current != upAxis) // if holding input and surface is not flat. the player is either on the ground or not on the ground ??
         {
             Vector3 direction = -upAxis;
-            if (playerCollision.IsGrounded.current)
+            if (playerCollision.IsGrounded.current) // if on ground, change the direction of groundpound
             {
                 Vector3 acrossSlope = Vector3.Cross(upAxis, playerCollision.ContactNormal.current);
                 Vector3 downSlope = Vector3.Cross(acrossSlope, playerCollision.ContactNormal.current);
                 direction = downSlope;
                 direction *= slopeDownForce;
             }
-            else
+            else // else maintain the regular ground pound direction
             {
                 direction *= downwardsForce;
             }
 
             rb.AddForce(direction * increasingForce.current, ForceMode.Acceleration);
-            increasingForce.current += deltaIncreaseForce;
+            increasingForce.current += deltaIncreaseForce; // the longer you are holding the input, the greater the groundpound force
         }
-        else
+        else // on exit
         {
-            groundPounding = false;
+            groundPounding = false; // can remove ??
             increasingForce.RevertCurrent();
         }
     }
