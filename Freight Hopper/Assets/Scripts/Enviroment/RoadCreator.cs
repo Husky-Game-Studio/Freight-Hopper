b24fa12d91611c01aac06a9baa7d09cb5a3732fe
Flootow <43686541+Flootow@users.noreply.github.com>
2021-07-02 17:26:17 -0700

    Road Component Ease
    New buttons on RoadCreator that add suggested components

--- Freight Hopper/Assets/Scripts/Enviroment/RoadCreator.cs
+++ Freight Hopper/Assets/Scripts/Enviroment/RoadCreator.cs
@@ -12,7 +12,7 @@ public class RoadCreator : MonoBehaviour
     public RoadSlice slice;
 
     [Range(1, 100)]
-    public int roadDetail;
+    public int roadDetail = 20;
 
     public void CreateRoad()
     {
@@ -21,7 +21,11 @@ public class RoadCreator : MonoBehaviour
             pathCreator = this.gameObject.GetComponent<PathCreator>();
             if (pathCreator == null)
             {
-                Debug.Log("No path creator found, please input one or add one as a component");
+                pathCreator = this.transform.parent.GetComponent<PathCreator>();
+                if (pathCreator == null)
+                {
+                    Debug.Log("No path creator found, please input one or add one as a component to this or the parent");
+                }
             }
         }
 
