27507ae1f5419252c109a61b5ccd182d2589cca0
Ayrton <ayrtonmuniz2002@gmail.com>
2021-07-13 02:19:05 -0700

    Wind doesn't crash the game

--- Freight Hopper/Assets/Scripts/Enviroment/Wind/ObjectScan.cs
+++ Freight Hopper/Assets/Scripts/Enviroment/Wind/ObjectScan.cs
@@ -10,9 +10,9 @@ public class ObjectScan
     private float rayWidth;
     private float depth;
     private Transform windTransform;
-    private Dictionary<Vector3, Ray> viableRays = new Dictionary<Vector3, Ray>();
+    private Dictionary<Vector3Int, Ray> viableRays = new Dictionary<Vector3Int, Ray>();
 
-    public Dictionary<Vector3, Ray> ViableRays => viableRays;
+    public Dictionary<Vector3Int, Ray> ViableRays => viableRays;
 
     public ObjectScan(Collider[] colliders, float rayWidth, Transform transform, float depth, Func<Vector3, bool> checkInsideWind)
     {
@@ -37,8 +37,54 @@ public class ObjectScan
         AddRays(origin);
     }
 
+    public bool IsViableRay(Ray ray)
+    {
+        if (viableRays.ContainsKey(ConvertVector3Int(ray.origin)) || !checkInsideWindFunction(ray.origin + (ray.direction / 100)))
+        {
+            return false;
+        }
+
+        RaycastHit[] hits = new RaycastHit[targetColliders.Length];
+        for (int j = 0; j < targetColliders.Length; j++)
+        {
+            targetColliders[j].Raycast(ray, out hits[j], depth);
+        }
+
+        bool hitCollider = false;
+        for (int j = 0; j < hits.Length; j++)
+        {
+            foreach (Collider collider in targetColliders)
+            {
+                if (collider == null)
+                {
+                    continue;
+                }
+                if (hits[j].collider == collider)
+                {
+                    hitCollider = true;
+                    break;
+                }
+            }
+            if (hitCollider)
+            {
+                break;
+            }
+        }
+
+        return hitCollider;
+    }
+
+    private Vector3Int ConvertVector3Int(Vector3 vector3)
+    {
+        return Vector3Int.RoundToInt(100 * vector3);
+    }
+
     private void AddRays(Ray rayOrigin)
     {
+        if (!IsViableRay(rayOrigin))
+        {
+            return;
+        }
         Queue<Ray> origins = new Queue<Ray>();
 
         Vector3[] directions =
@@ -50,8 +96,7 @@ public class ObjectScan
         };
         Ray w = new Ray(rayOrigin.origin, rayOrigin.direction);
         origins.Enqueue(w);
-        viableRays.Add(w.origin, w);
-
+        viableRays.Add(ConvertVector3Int(w.origin), w);
         while (origins.Count > 0)
         {
             w = origins.Dequeue();
@@ -59,42 +104,13 @@ public class ObjectScan
             for (int i = 0; i < directions.Length; i++)
             {
                 Ray ray = new Ray(w.origin + (directions[i] * rayWidth), rayOrigin.direction);
-                //Debug.DrawRay(ray.origin, ray.direction * 20, Color.red);
-                if (viableRays.ContainsKey(ray.origin) || !checkInsideWindFunction(ray.origin + (ray.direction / 100)))
-                {
-                    continue;
-                }
-
-                RaycastHit[] hits = new RaycastHit[targetColliders.Length];
-                for (int j = 0; j < targetColliders.Length; j++)
-                {
-                    targetColliders[j].Raycast(ray, out hits[j], depth);
-                }
-
-                bool hitCollider = false;
-                for (int j = 0; j < hits.Length; j++)
-                {
-                    foreach (Collider collider in targetColliders)
-                    {
-                        if (hits[j].collider == collider)
-                        {
-                            hitCollider = true;
-                            break;
-                        }
-                    }
-                    if (hitCollider)
-                    {
-                        break;
-                    }
-                }
-
-                if (!hitCollider)
+                if (!IsViableRay(ray))
                 {
                     continue;
                 }
 
                 origins.Enqueue(ray);
-                viableRays.Add(ray.origin, ray);
+                viableRays.Add(ConvertVector3Int(ray.origin), ray);
             }
         }
     }
--- Freight Hopper/Assets/Scripts/Enviroment/Wind/Wind.cs
+++ Freight Hopper/Assets/Scripts/Enviroment/Wind/Wind.cs
@@ -232,11 +232,11 @@ public class Wind : MonoBehaviour
                     }
                     affectedBodies[colliderRb].Add(new Ray(hit.point, ray.direction));
 
-                    Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.blue);
+                    //Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.blue);
                 }
                 else
                 {
-                    Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.green);
+                    //Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.green);
                 }
             }
         }
