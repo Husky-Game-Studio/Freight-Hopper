5783c4018002be2ede3b37bbd79644ad5343120c
lycheefuzz <ninapan118@hotmail.com>
2021-07-28 11:13:49 -0700

    Load Menu - Deletion works!

--- Freight Hopper/Assets/Scripts/LevelEditorScripts/UI Things/LoadLevelUI.cs
+++ Freight Hopper/Assets/Scripts/LevelEditorScripts/UI Things/LoadLevelUI.cs
@@ -4,27 +4,52 @@ using UnityEngine;
 using UnityEngine.UI; 
 using HGSLevelEditor;
 using UnityEngine.SceneManagement;
+using UnityEngine.Windows;
 
 public class LoadLevelUI : MonoBehaviour
 {
-    [SerializeField]
-    private Text txt;
+    [SerializeField] private Text txt;
     public string levelName;
 
+    [SerializeField] private GameObject validatingMenu;
+
+    
+    public void DeleteLevel() {
+
+        File.Delete(Application.streamingAssetsPath + "/Levels" + "/" + LevelManager.levelNameLoad);
+        GridLoadLevelUI.GetInstance().ReloadLevels();
+        SetMenu();
+    }
     public void LoadLevel() {
 
         //Need this for when I'm going back to the first iteration of the level editor 
         // SceneManager.LoadScene("LevelEditorBeginning");
         SceneManager.LoadScene("LevelEditorIteration3");
-        SaveLoadLevel.GetInstance().LoadButton(levelName, true);
+        SaveLoadLevel.GetInstance().LoadButton(LevelManager.levelNameLoad, true);
         GridLoadLevelUI.GetInstance().CloseLoadLevelButtons();
-        LevelManager.levelNameLoad = levelName;
+        //LevelManager.levelNameLoad = levelName;
     }
     public void SetText(string newText) {
-
-
         txt.text = newText; 
-    
+    }
+
+    public void SetLevelName() {
+
+        LevelManager.levelNameLoad = levelName;
+        Debug.Log ("levelName selected: " + LevelManager.levelNameLoad);
+    }
+    public void SetMenu() {
+
+        if (validatingMenu.activeInHierarchy == false) {
+
+            validatingMenu.SetActive(true);
+        }
+        else {
+
+            validatingMenu.SetActive(false);
+        }
+       
+
     }
 
 
