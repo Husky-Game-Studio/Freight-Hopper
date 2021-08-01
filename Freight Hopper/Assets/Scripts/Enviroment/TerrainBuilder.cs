6276e2bd21dd78c276374a577dc7ad7a652ec80c
Ayrton <ayrtonmuniz2002@gmail.com>
2021-07-31 23:27:55 -0700

    Terrain Undo

--- Freight Hopper/Assets/Scripts/Enviroment/TerrainBuilder.cs
+++ Freight Hopper/Assets/Scripts/Enviroment/TerrainBuilder.cs
@@ -3,34 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+[ExecuteInEditMode]
 public class TerrainBuilder : MonoBehaviour
 {
 #if UNITY_EDITOR
     [SerializeField] private GameObject terrain;
-    private List<GameObject> builtTerrain = new List<GameObject>();
+    [SerializeField] private List<GameObject> builtTerrain = new List<GameObject>();
     [SerializeField] private string terrainName = "Terrain";
     [SerializeField] private FloatConst terrainWidth;
-    [SerializeField] private Vector2 size;
+    [SerializeField] private Vector2Int size;
 
     [ContextMenu("Generate")]
     public void Generate()
     {
-        for (int i = 0; i < builtTerrain.Count; i++)
-        {
-            DestroyImmediate(builtTerrain[i]);
-        }
-        builtTerrain.Clear();
-        Vector3 position = this.transform.position;
+        Clear();
+
         for (int x = 0; x < size.x; x++)
         {
             for (int y = 0; y < size.y; y++)
             {
                 GameObject go = PrefabUtility.InstantiatePrefab(terrain, this.transform) as GameObject;
+                Undo.RegisterCreatedObjectUndo(go, "Generate Terrain");
                 go.name = terrainName + " " + x + ", " + y;
-                go.transform.position = position + (this.transform.right * x * terrainWidth.Value) + (this.transform.forward * y * terrainWidth.Value);
+                go.transform.position = this.transform.position + (this.transform.right * x * terrainWidth.Value) + (this.transform.forward * y * terrainWidth.Value);
                 builtTerrain.Add(go);
             }
         }
+    }
+
+    [ContextMenu("Clear")]
+    public void Clear()
+    {
+        foreach (GameObject go in builtTerrain)
+        {
+            if (go != null)
+            {
+                Undo.DestroyObjectImmediate(go);
+            }
+        }
+
+        builtTerrain.Clear();
     }
 
 #endif
