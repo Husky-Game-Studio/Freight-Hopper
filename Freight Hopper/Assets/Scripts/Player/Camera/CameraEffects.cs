e6e6976fe3ef80f2924b1c8232ab1b4aca707e8b
Ayrton <ayrtonmuniz2002@gmail.com>
2021-08-02 02:19:44 -0700

    Fixed B2-002 Speed Particle Turbulence

--- Freight Hopper/Assets/Scripts/Player/Camera/CameraEffects.cs
+++ Freight Hopper/Assets/Scripts/Player/Camera/CameraEffects.cs
@@ -12,13 +12,11 @@ public class CameraEffects : MonoBehaviour
     private VisualEffect speedLines;
 
     [SerializeField] private Volume speedVolume;
-    [SerializeField] private float speedEffectsStart = 15;
-    private Average playerSpeed;
+    [SerializeField] private float speedEffectsStart = 50;
+    [SerializeField] private float speedEffectsEnd = 120;
 
     [SerializeField] private SoundManager playerSounds;
     [SerializeField] private CameraEffect<float> fov;
-    [SerializeField] private CameraEffect<float> tilt;
-    [SerializeField] private CameraEffect<float> postProcessing;
 
     [System.Serializable]
     public struct CameraEffect<T>
@@ -33,20 +31,15 @@ public class CameraEffects : MonoBehaviour
     private void Awake()
     {
         playerRB = GameObject.FindGameObjectWithTag("Player").gameObject.GetComponent<Rigidbody>();
-        playerSpeed = new Average(300);
 
         cam = Camera.main;
 
         fov.baseValue = cam.fieldOfView;
         fov.value = fov.baseValue;
-
-        postProcessing.baseValue = 0;
-        postProcessing.value = postProcessing.baseValue;
-
-        tilt.baseValue = cam.transform.rotation.eulerAngles.z;
-        tilt.value = tilt.baseValue;
+        fov.maxValue += fov.baseValue;
 
         speedLines = Camera.main.GetComponent<VisualEffect>();
+
         speedLines.Stop();
     }
 
@@ -54,26 +47,18 @@ public class CameraEffects : MonoBehaviour
 
     private void Update()
     {
-        Vector3 speedLineDirection = cam.transform.InverseTransformDirection(playerRB.velocity.normalized);
-        speedLines.SetVector3("Direction", speedLineDirection);
-        playerSpeed.Update(playerRB.velocity.magnitude);
-        if (playerRB.velocity.magnitude <= 1)
-        {
-            playerSpeed.Reset();
-        }
+        Vector3 speedLineDirection = playerRB.transform.InverseTransformDirection(playerRB.velocity.normalized);
+        speedLines.SetVector3("Direction", playerRB.velocity.normalized);
+        float speed = playerRB.velocity.magnitude;
 
-        fov.lerpValue = Mathf.Clamp((playerSpeed.GetAverage() - speedEffectsStart) / speedEffectsStart, 0, 1);
+        fov.lerpValue = Mathf.Clamp((speed - speedEffectsStart) / speedEffectsEnd, 0, 1);
         fov.value = Mathf.Lerp(fov.baseValue, fov.maxValue, fov.lerpValue);
 
-        //cam.transform.rotation = Quaternion.Euler(0, 0, tilt.value);
-
-        postProcessing.lerpValue = Mathf.Clamp((playerSpeed.GetAverage() - speedEffectsStart) / speedEffectsStart, 0, 1);
-        postProcessing.value = Mathf.Lerp(postProcessing.baseValue, postProcessing.maxValue, postProcessing.lerpValue);
+        speedLines.SetFloat("Scalar", fov.lerpValue + 1);
 
         cam.fieldOfView = fov.value;
-        speedVolume.weight = postProcessing.value;
 
-        if (playerSpeed.GetAverage() > speedEffectsStart)
+        if (speed > speedEffectsStart)
         {
             speedLines.Play();
             playerSounds.Play("GoingFast");
@@ -83,44 +68,19 @@ public class CameraEffects : MonoBehaviour
         {
             speedLines.Stop();
         }
-        if (goingSlowAgain && playerSpeed.GetAverage() < speedEffectsStart)
+        if (goingSlowAgain && speed < speedEffectsStart)
         {
             playerSounds.Stop("GoingFast");
             goingSlowAgain = false;
         }
     }
 
-    /// <summary>
-    /// Tilts the player the given amount of degrees
-    /// </summary>
-    /// <param name="amount"></param>
-    public void Tilt(float amount)
+    private Color BlendGradients(Gradient a, Gradient b, float bias, float t)
     {
-        StartCoroutine(Tilting(amount));
-    }
+        Color fromA = a.Evaluate(t);
+        Color fromB = b.Evaluate(t);
 
-    /// <summary>
-    /// Resets the tilt of the player back to the base value
-    /// </summary>
-    public void ResetTilt()
-    {
-        StartCoroutine(Tilting(tilt.baseValue - tilt.value));
-    }
-
-    private IEnumerator Tilting(float amount)
-    {
-        float startingValue = tilt.value;
-        float endingValue = tilt.value + amount;
-
-        tilt.lerpValue = 0;
-        while (tilt.lerpValue <= 1)
-        {
-            tilt.lerpValue += 0.01f;
-
-            tilt.value = Mathf.Lerp(tilt.value, startingValue, endingValue);
-
-            //cam.transform.rotation = Quaternion.Euler(0, 0, tilt.value);
-            yield return null;
-        }
+        Color final = Color.Lerp(fromA, fromB, bias);
+        return final;
     }
 }
