967f5679c6d4056edb137118fd5d0a49f1b37acf
Gabriel O <oliver.gabriel.a@hotmail.com>
2021-08-06 18:30:56 -0700

    bullet has collision and rb

--- Freight Hopper/Assets/Scripts/Turret/BulletBehavior.cs
+++ Freight Hopper/Assets/Scripts/Turret/BulletBehavior.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class BulletBehavior : MonoBehaviour
 {
+    private Rigidbody myRB;
     // Start is called before the first frame update
     void Start()
     {
         Destroy(this.gameObject, 4f);
+        myRB = this.gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //myRB.AddForce(0, 0, 100f);
     }
 }
