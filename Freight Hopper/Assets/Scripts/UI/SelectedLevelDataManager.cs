8d33611f23aa243af422d9f8caad72f7b4a39dbb
Ayrton <ayrtonmuniz2002@gmail.com>
2021-10-08 13:45:43 -0700

    Weird One Way UI Binding For Ability Changing

--- Freight Hopper/Assets/Scripts/UI/SelectedLevelDataManager.cs
+++ Freight Hopper/Assets/Scripts/UI/SelectedLevelDataManager.cs
@@ -17,40 +17,54 @@ public class SelectedLevelDataManager : MonoBehaviour
 
     public LevelData CurrentLevelData => currentWorld.Levels[LevelSelectLevelButton.currentID - 1];
 
+    public void UpdateUI()
+    {
+        LevelData currentData = this.CurrentLevelData;
+        //Debug.Log(LevelSelectLevelButton.currentID - 1);
+        worldtitle.text = currentWorld.name;
+        title.text = currentData.Title;
+        LevelTimeSaveData levelSaveData = LevelTimeSaveLoader.Load(currentData.name);
+        if (levelSaveData != null)
+        {
+            if (levelSaveData.GetTime(currentData.ActiveAbilities).Item2 == -1)
+            {
+                bestTime.text = "Best: None";
+            }
+            else
+            {
+                bestTime.text = "Best: " + LevelTimer.GetTimeString(levelSaveData.GetTime(currentData.ActiveAbilities).Item1);
+            }
+
+            if (levelSaveData.MedalIndex >= 0)
+            {
+                medal.gameObject.SetActive(true);
+                medal.sprite = medalSprites.Sprites[levelSaveData.MedalIndex];
+            }
+            else
+            {
+                medal.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            bestTime.text = "Best: None";
+            medal.gameObject.SetActive(false);
+        }
+        levelImage.texture = currentData.Image;
+
+        Button button = levelImage.GetComponent<Button>();
+        if (button.onClick.GetPersistentEventCount() > 0)
+        {
+            button.onClick.RemoveAllListeners();
+        }
+        button.onClick.AddListener(delegate { SceneLoader.LoadLevel(currentData.name); });
+    }
+
     private void Update()
     {
         if (LevelSelectLevelButton.currentID != lastIndex)
         {
-            LevelData currentData = this.CurrentLevelData;
-            worldtitle.text = currentWorld.name;
-            title.text = currentData.Title;
-            LevelTimeSaveData levelSaveData = LevelTimeSaveLoader.Load(currentData.name);
-            if (levelSaveData != null)
-            {
-                bestTime.text = "Best: " + LevelTimer.GetTimeString(levelSaveData.GetTime(currentData.ActiveAbilities).Item1);
-                if (levelSaveData.MedalIndex >= 0)
-                {
-                    medal.gameObject.SetActive(true);
-                    medal.sprite = medalSprites.Sprites[levelSaveData.MedalIndex];
-                }
-                else
-                {
-                    medal.gameObject.SetActive(false);
-                }
-            }
-            else
-            {
-                bestTime.text = "No Best Time";
-                medal.gameObject.SetActive(false);
-            }
-            levelImage.texture = currentData.Image;
-
-            Button button = levelImage.GetComponent<Button>();
-            if (button.onClick.GetPersistentEventCount() > 0)
-            {
-                button.onClick.RemoveAllListeners();
-            }
-            button.onClick.AddListener(delegate { SceneLoader.LoadLevel(currentData.name); });
+            UpdateUI();
         }
         lastIndex = LevelSelectLevelButton.currentID;
     }
