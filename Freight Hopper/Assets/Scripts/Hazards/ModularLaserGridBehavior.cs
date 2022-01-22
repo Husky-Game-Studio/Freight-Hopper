9b62e30a224d330ca73aa2a8f30442cc86aa4138
Gabriel O <olivergabe@hotmail.com>
2022-01-21 17:06:09 -0800

    laser grid support objects settup

--- Freight Hopper/Assets/Scripts/Hazards/ModularLaserGridBehavior.cs
+++ Freight Hopper/Assets/Scripts/Hazards/ModularLaserGridBehavior.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class ModularLaserGridBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private GameObject poleObject;
+    [SerializeField] private GameObject laserObject;
+    
+    
 }
