8885c10dbb8fb622f6106602abd81b6175bd7bca
Ayrton <ayrtonmuniz2002@gmail.com>
2022-01-28 15:25:27 -0800

    Refactored PlayerDeath.cs
    I got rid of the rigidbody and collider. It wasn't needed, we now only check inbetween frames if we hit anything.

--- Freight Hopper/Assets/Scripts/Player/PlayerDeath.cs
+++ Freight Hopper/Assets/Scripts/Player/PlayerDeath.cs
@@ -1,30 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerDeath : MonoBehaviour
 {
-    private Rigidbody rb;
     private Memory<Vector3> position;
     [SerializeField] private Transform head;
-    [SerializeField] private SphereCollider deathCollider;
+    [SerializeField] private MeshRenderer playerMeshRenderer;
+    [SerializeField] private float radius;
     [SerializeField] private LayerMask deathMask;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(head.position, radius);
+    }
 
     private void Awake()
     {
-        rb = this.transform.GetComponent<Rigidbody>();
-        position.current = head.transform.position;
+        position.SetCurrent(head.transform.position);
         position.UpdateOld();
     }
 
     private void CheckInBetweenCollision()
     {
-        position.current = head.transform.position;
+        position.SetCurrent(head.transform.position);
         Vector3 dif = position.current - position.old;
         float dist = dif.magnitude;
         Vector3 dir = dif.normalized;
-        if (Physics.SphereCast(position.old, deathCollider.radius, dir, out _, dist, deathMask))
+        // Inbetween fixed updates, the player has essentially teleported. This sphere cast will
+        // check inbetween the teleportations in a radius to see if we have hit anything
+        if (Physics.SphereCast(position.old, radius, dir, out _, dist, deathMask))
         {
+            head.parent.transform.position = position.old; // This is to move the camera back to the old position so that its not inside walls (as much)
+            playerMeshRenderer.enabled = false; // If we don't do this then the player sees themself when they die. It looks odd
             KillPlayer();
         }
         position.UpdateOld();
@@ -32,35 +38,11 @@ public class PlayerDeath : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.MovePosition(head.transform.position);
         CheckInBetweenCollision();
     }
 
     public void KillPlayer()
     {
         LevelController.Instance.Respawn();
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        CheckDeath(collision);
-    }
-
-    private void OnCollisionStay(Collision collision)
-    {
-        CheckDeath(collision);
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        CheckDeath(collision);
-    }
-
-    private void CheckDeath(Collision collision)
-    {
-        if (!collision.gameObject.CompareTag("landable"))
-        {
-            KillPlayer();
-        }
     }
 }
