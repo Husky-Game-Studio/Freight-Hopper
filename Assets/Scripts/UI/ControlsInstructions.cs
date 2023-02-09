b3d0f812e581ad61effe83de8427da75c010e57e
Gabriel O <olivergabe@hotmail.com>
2023-02-09 00:26:41 -0800

    modified tutorial texts (1-1 to 1-4). deleted unused tutorial and main menu images

--- Assets/Scripts/UI/ControlsInstructions.cs
+++ Assets/Scripts/UI/ControlsInstructions.cs
@@ -1,10 +1,12 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ControlsInstructions : MonoBehaviour
 {
     [SerializeField] private Image tutorialImage;
+    [SerializeField] private TextMeshProUGUI tutorialText;
     private const float fadeInTime = 0.8f;
 
     private void Start()
@@ -14,30 +16,36 @@ public class ControlsInstructions : MonoBehaviour
         if (levelTimeSaveData == null)
         {
             tutorialImage.enabled = true;
+            tutorialText.enabled = true;
         }
         else
         {
             if (levelTimeSaveData.MedalIndex > 0)
             {
                 tutorialImage.enabled = false;
+                tutorialText.enabled = false;
             }
             else
             {
                 tutorialImage.enabled = true;
+                tutorialText.enabled = true;
             }
         }
 
         if(tutorialImage.enabled) 
         {
             tutorialImage.color = new Color(tutorialImage.color.r, tutorialImage.color.g, tutorialImage.color.b, 0f);
+            tutorialImage.color = new Color(tutorialText.color.r, tutorialText.color.g, tutorialText.color.b, 0f);
             StartCoroutine(FadeIn());
         }
     }
 
     private IEnumerator FadeIn()
     {
-        Color initialColor = tutorialImage.color;   
+        Color initialColor = tutorialImage.color;
+        Color initialColorText = tutorialText.color;
         Color targetColor = new Color(initialColor.r, initialColor.g, initialColor.b, 1f);
+        Color targetColorText = new Color(initialColorText.r, initialColorText.g, initialColorText.b, 1f);
 
         float elapsedTime = 0f;
 
@@ -45,6 +53,7 @@ public class ControlsInstructions : MonoBehaviour
         {
             elapsedTime += Time.deltaTime;
             tutorialImage.color = Color.Lerp(initialColor, targetColor, elapsedTime / fadeInTime);
+            tutorialText.color = Color.Lerp(initialColorText, targetColorText, elapsedTime / fadeInTime);
             yield return null;
         }
     }
