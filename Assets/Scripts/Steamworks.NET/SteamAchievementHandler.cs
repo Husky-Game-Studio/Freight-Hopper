d408410e64cd054e1c52092bfbf4966ea8ab4650
Dilan Mac <dilan.mac@gmail.com>
2023-02-11 19:32:40 -0800

    steam achievements functionality updated to remove bound checking, which is broken courtesy of steam

--- Assets/Scripts/Steamworks.NET/SteamAchievementHandler.cs
+++ Assets/Scripts/Steamworks.NET/SteamAchievementHandler.cs
@@ -11,28 +11,60 @@ namespace SteamTrain
         private StatAchieveMap mapSO; 
         public static Dictionary<string, string[]> stat_achievementMap = new Dictionary<string, string[]>();
         public static bool statsRetrieved { get; private set; } = false;
+        public int testCase = 1;
 
         void Start()
         {
             if (SteamManager.Initialized)
             {
+                Debug.Log(SteamFriends.GetPersonaName());
                 statsRetrieved = SteamUserStats.RequestCurrentStats();
                 if (statsRetrieved)
                     foreach (StatAchieveMap.AchievementStats a in mapSO.map)
+                    {
                         stat_achievementMap[a.stat] = a.achievements.ToArray();
+                        int man;
+                        switch (testCase)
+                        {
+                            // give me half the progress of all the achievements
+                            case 1:
+                                GetStat(a.stat, out man);
+                                Debug.Log(man);
+                                Debug.Log("Set progress to 9 in all the stats.");
+                                Debug.Log(SetStatsAndCheckAchievements(a.stat, 9));
+                                break;
+                            // give me all the progress of all the achievements
+                            case 2:
+                                GetStat(a.stat, out man);
+                                Debug.Log(man);
+                                Debug.Log("Set progress to 10 in all the stats.");
+                                Debug.Log(SetStatsAndCheckAchievements(a.stat, 10));
+                                break;
+                            // give me the achievements
+                            case 3:
+                                Debug.Log("Unlock index one of all achievements.");
+                                Debug.Log(UnlockAchievement(a.achievements[0]));
+                                break;
+                            // remove me the achievements
+                            case 4:
+                                Debug.Log("Delete index one of all achievements.");
+                                Debug.Log(ClearAchievement(a.achievements[0]));
+                                break;
+                        }
+                    }
             }
         }
 
         public static bool UnlockAchievement(string id)
         {
             bool flag;
-            SteamUserStats.GetAchievement(id, out flag);
+            Debug.Log(SteamUserStats.GetAchievement(id, out flag));
             if (!flag)
                if(SteamUserStats.SetAchievement(id))
                     // this has a callback, which should probably be tracked somewhere for errors
                    return SteamUserStats.StoreStats();
 
-            Debug.Log("This achivement does not exist, or is not published.");
+            Debug.Log("This achivement does not exist, or is not published, or the ID is wrong.");
             return false;
         }
 
@@ -68,18 +100,27 @@ namespace SteamTrain
             {
                 foreach (string s in stat_achievementMap[id])
                 {
-                    int minProg;
-                    int maxProg;
                     // technically would be more optimal to just set instead of get
                     // however, the get is necessary if I want to prioritize the steam interface
+                    Debug.Log(s);
+                    // i have no idea why, but this is no longer working https://github.com/rlabrecque/Steamworks.NET/issues/448
+                    /*
+                    int minProg;
+                    int maxProg;
                     if (SteamUserStats.GetAchievementProgressLimits(s, out minProg, out maxProg))
+                    {
+                        Debug.Log(maxProg);
                         if (progress >= maxProg)
                             Debug.Log(UnlockAchievement(s));
+                    }*/
+                    if (progress >= 10)
+                        Debug.Log(UnlockAchievement(s));
                 }
                 
             }
             if (SteamUserStats.SetStat(id, progress) && request)
                 return SteamUserStats.StoreStats();
+            Debug.Log("Stat failed to set.");
             return false;
         }
 
