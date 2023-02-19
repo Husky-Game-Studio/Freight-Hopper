72e8bec9e67b57fa520c752bcfa29342a828197e
Dilan Mac <dilan.mac@gmail.com>
2023-02-18 17:05:48 -0800

    Leaderboard accounting for more than 1 leaderboard per level

--- Assets/Scripts/EventLogging/LeaderboardEventHandler.cs
+++ Assets/Scripts/EventLogging/LeaderboardEventHandler.cs
@@ -1,21 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Steamworks;
 
 public class LeaderboardEventHandler : MonoBehaviour
-{   
+{
+    public List<string> leaderboards = new List<string>();
+
+    private Dictionary<string, SteamTrain.SteamLeaderboardHandler> leaderboardHandlers = new Dictionary<string, SteamTrain.SteamLeaderboardHandler>();
+
     private void Start()
     {
-        EventBoat.NewBestTime += BeginTimeUploadingProcess;
+        foreach(string s in leaderboards)
+        {
+            leaderboardHandlers[s] = new SteamTrain.SteamLeaderboardHandler();
+            leaderboardHandlers[s].FindLeaderboard(s);
+        }
+        EventBoat.NewBestTime += UploadTimes;
     }
 
-    public void FindLeaderboard(string level)
+    public void UploadTimes(string level, float time)
     {
-        SteamTrain.SteamLeaderboardHandler.FindLeaderboard(level);
+        foreach(string s in leaderboards)
+        {
+            Debug.Log(s + leaderboardHandlers[s].UploadTimeBestTime(time, OnUploadScore).ToString());
+        }
     }
 
-    public void BeginTimeUploadingProcess(string level, float time)
+    public void OnUploadScore(LeaderboardScoreUploaded_t r, bool failure)
     {
-        SteamTrain.SteamLeaderboardHandler.UploadTimeBestTime(time);
+        if (r.m_bSuccess == 1 && !failure)
+        {
+            Debug.Log("Score uploaded.");
+            if (r.m_bScoreChanged == 1)
+                Debug.Log("Updated score.");
+        }
+        else
+            Debug.Log("Failed to upload score.");
+    }
+
+    public void OnDownloadScore(LeaderboardScoresDownloaded_t r, bool failure)
+    {
+        if (r.m_cEntryCount > 0 && !failure)
+        {
+            Debug.Log("Scores downloaded.");
+        }
+        else
+            Debug.Log("Failed to download scores.");
     }
 }
--- Assets/Scripts/Steamworks.NET/SteamLeaderboardHandler.cs
+++ Assets/Scripts/Steamworks.NET/SteamLeaderboardHandler.cs
@@ -9,95 +9,71 @@ namespace SteamTrain
     // https://partner.steamgames.com/doc/features/leaderboards/guide
     public class SteamLeaderboardHandler
     {
-        private static SteamLeaderboard_t m_CurrentLeaderboard;
-        public static bool foundLeaderboard { get; private set; } = false;
+        public SteamLeaderboard_t currentLeaderboard { get; private set; }
+        private CallResult<LeaderboardFindResult_t> callResultFindLeaderboard = new CallResult<LeaderboardFindResult_t>();
+        private CallResult<LeaderboardScoreUploaded_t> callResultUploadScore = new CallResult<LeaderboardScoreUploaded_t>();
+        private CallResult<LeaderboardScoresDownloaded_t> callResultDownloadScore = new CallResult<LeaderboardScoresDownloaded_t>();
 
-        private static CallResult<LeaderboardFindResult_t> m_callResultFindLeaderboard = new CallResult<LeaderboardFindResult_t>();
-        private static CallResult<LeaderboardScoreUploaded_t> m_callResultUploadScore = new CallResult<LeaderboardScoreUploaded_t>();
-        private static CallResult<LeaderboardScoresDownloaded_t> m_callResultDownloadScore = new CallResult<LeaderboardScoresDownloaded_t>();
+        public bool foundLeaderboard { get; private set; } = false;
 
-        public static void FindLeaderboard(string name)
+        public void FindLeaderboard(string name)
         {
-            foundLeaderboard = false;
             SteamAPICall_t cb = SteamUserStats.FindLeaderboard(name);
-            m_callResultFindLeaderboard.Set(cb, OnFindLeaderboard);
+            callResultFindLeaderboard.Set(cb, OnFindLeaderboard);
         }
 
-        public static bool UploadTimeBestTime(float time)
+        public bool UploadTimeBestTime(float time, CallResult<LeaderboardScoreUploaded_t>.APIDispatchDelegate del)
         {
             if(foundLeaderboard)
             {
                 // makes sure that it only keeps best
-                SteamAPICall_t cb = SteamUserStats.UploadLeaderboardScore(m_CurrentLeaderboard,
+                SteamAPICall_t cb = SteamUserStats.UploadLeaderboardScore(currentLeaderboard,
                                                                           ELeaderboardUploadScoreMethod.k_ELeaderboardUploadScoreMethodKeepBest,
                                                                           (int)time, null, 0);
-                m_callResultUploadScore.Set(cb, OnUploadScore);
+                callResultUploadScore.Set(cb, del);
                 return true;
             }
             return false;
         }
 
-        public static bool UploadTimeForced(float time)
+        public bool UploadTimeForced(float time, CallResult<LeaderboardScoreUploaded_t>.APIDispatchDelegate del)
         {
             if (foundLeaderboard)
             {
                 // makes sure that it forcibly updates
-                SteamAPICall_t cb = SteamUserStats.UploadLeaderboardScore(m_CurrentLeaderboard,
+                SteamAPICall_t cb = SteamUserStats.UploadLeaderboardScore(currentLeaderboard,
                                                                           ELeaderboardUploadScoreMethod.k_ELeaderboardUploadScoreMethodForceUpdate,
                                                                           (int)time, null, 0);
-                m_callResultUploadScore.Set(cb, OnUploadScore);
+                callResultUploadScore.Set(cb, del);
                 return true;
             }
             return false;
         }
 
-        public static bool DownloadScores()
+        public bool DownloadScores(CallResult<LeaderboardScoresDownloaded_t>.APIDispatchDelegate del)
         {
             if (foundLeaderboard)
             {
-                SteamAPICall_t cb = SteamUserStats.DownloadLeaderboardEntries(m_CurrentLeaderboard,
+                SteamAPICall_t cb = SteamUserStats.DownloadLeaderboardEntries(currentLeaderboard,
                                                                           ELeaderboardDataRequest.k_ELeaderboardDataRequestGlobal,
                                                                           1, 10);
-                m_callResultDownloadScore.Set(cb, OnDownloadScore);
+                callResultDownloadScore.Set(cb, del);
                 return true;
             }
             return false;
         }
 
-        public static void OnFindLeaderboard(LeaderboardFindResult_t r, bool failure)
+        private void OnFindLeaderboard(LeaderboardFindResult_t r, bool failure)
         {
             // no operations can be done if the target leaderboard has not been found, so this requires a flag
             foundLeaderboard = true;
             if (r.m_bLeaderboardFound == 1 && !failure)
             {
                 Debug.Log("Found leaderboard.");
-                m_CurrentLeaderboard = r.m_hSteamLeaderboard;
+                currentLeaderboard = r.m_hSteamLeaderboard;
             }
             else
                 Debug.Log("Failed to find leaderboard.");
         }
-
-        public static void OnUploadScore(LeaderboardScoreUploaded_t r, bool failure)
-        {
-            if (r.m_bSuccess == 1 && !failure)
-            {
-                Debug.Log("Score uploaded.");
-                if (r.m_bScoreChanged == 1)
-                    Debug.Log("Updated score.");
-            }
-            else
-                Debug.Log("Failed to upload score.");
-        }
-
-        public static void OnDownloadScore(LeaderboardScoresDownloaded_t r, bool failure)
-        {
-            if (r.m_cEntryCount > 0 && !failure)
-            {
-                Debug.Log("Scores downloaded.");
-            }
-            else
-                Debug.Log("Failed to download scores.");
-        }
-
     }
 }
