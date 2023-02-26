c46755eedd7d847ad843674dc740fe0c699bfb2c
AJ213 <ayrtonmuniz2002@gmail.com>
2023-02-25 17:33:09 -0800

    added loop scroll rect and implemented leaderboard UI thingy

--- Assets/Scripts/UILeaderboard.cs
+++ Assets/Scripts/UILeaderboard.cs
@@ -1,8 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SteamTrain;
+using UnityEngine.UI;
 
-public class UILeaderboard : MonoBehaviour
+public class UILeaderboard : MonoBehaviour, LoopScrollPrefabSource, LoopScrollDataSource
 {
-    GameObject loadingIcon;
+    [SerializeField] GameObject loadingIcon;
+
+    [SerializeField] GameObject leaderboardEntryPrefab;
+    [SerializeField] LoopScrollRect loopScrollRect;
+    [SerializeField] UnityEngine.UI.Toggle toggleFriend;
+    [SerializeField] int timesToGrab = 50;
+
+    List<LeaderboardEntry> fullLeaderboard = new List<LeaderboardEntry>();
+    List<LeaderboardEntry> friendLeaderboard = new List<LeaderboardEntry>();
+    public bool IsFriendLeaderboard => toggleFriend.isOn;
+    public static UILeaderboard Instance;
+
+    void OnEnable(){
+        Instance = this;
+
+        loopScrollRect.prefabSource = this;
+        loopScrollRect.dataSource = this;
+    }
+
+    public IEnumerator LoadRelativeLeaderboard(string level){
+        loadingIcon.gameObject.SetActive(true);
+        yield return LeaderboardEventHandler.GetRelativeTimes(level, timesToGrab, fullLeaderboard, friendLeaderboard);
+        loadingIcon.gameObject.SetActive(false);
+
+        RefreshLeaderboard();
+    }
+
+    public void RefreshLeaderboard(){
+        int count = fullLeaderboard.Count;
+        if (IsFriendLeaderboard)
+        {
+            count = friendLeaderboard.Count;
+        }
+
+        loopScrollRect.totalCount = count;    
+        loopScrollRect.RefillCells();
+    }
+
+    public LeaderboardEntry GetEntry(int index){
+        if(IsFriendLeaderboard)
+        {
+            if (index >= friendLeaderboard.Count) return null;
+            return friendLeaderboard[index];
+        }
+        if (index >= fullLeaderboard.Count) return null;
+        return fullLeaderboard[index];
+    }
+    
+
+    // Implement your own Cache Pool here. The following is just for example.
+    Stack<Transform> pool = new Stack<Transform>();
+    public GameObject GetObject(int index)
+    {
+        if (pool.Count == 0)
+        {
+            return Instantiate(leaderboardEntryPrefab);
+        }
+        Transform candidate = pool.Pop();
+        candidate.gameObject.SetActive(true);
+        return candidate.gameObject;
+    }
+
+    public void ReturnObject(Transform trans)
+    {
+        // Use `DestroyImmediate` here if you don't need Pool
+        trans.SendMessage("ScrollCellReturn", SendMessageOptions.DontRequireReceiver);
+        trans.gameObject.SetActive(false);
+        trans.SetParent(transform, false);
+        pool.Push(trans);
+    }
+
+    public void ProvideData(Transform transform, int idx)
+    {
+        transform.SendMessage("ScrollCellIndex", idx);
+    }
 }
--- Assets/Scripts/UILeaderboardEntry.cs
+++ Assets/Scripts/UILeaderboardEntry.cs
@@ -1,8 +1,6 @@
 using SteamTrain;
 using UnityEngine;
 using TMPro;
-using System.Collections.Generic;
-using System.Collections;
 
 public class UILeaderboardEntry : MonoBehaviour
 {
@@ -10,9 +8,24 @@ public class UILeaderboardEntry : MonoBehaviour
     [SerializeField] TextMeshProUGUI user;
     [SerializeField] TextMeshProUGUI time;
 
+    bool hasData = false;
+
+    public void Reset(){
+        hasData = false;
+    }
     public void SetEntry(LeaderboardEntry data){
         rank.text = data.rank.ToString();
         user.text = data.playerName;
         time.text = LevelTimer.GetTimeString(data.timeSeconds);
+        hasData = true;
+    }
+    public void ScrollCellIndex(int idx)
+    {
+        LeaderboardEntry entry = UILeaderboard.Instance.GetEntry(idx);
+        if (entry != null) {
+            SetEntry(UILeaderboard.Instance.GetEntry(idx));
+        }else {
+            SetEntry(new LeaderboardEntry());
+        }
     }
 }
