d530aaa441b1b8f09c4207d9020b656f5de2fc97
AJ213 <ayrtonmuniz2002@gmail.com>
2023-02-25 21:44:00 -0800

    changed event boat information

--- Assets/Scripts/EventLogging/EventBoat.cs
+++ Assets/Scripts/EventLogging/EventBoat.cs
@@ -3,17 +3,28 @@ using System;
 // It just triggers these events with the information. Usually level
 public static class EventBoat
 {
-    // Sends level name "Format: WorldID LevelID" Assume World ID and Level ID is a string
-    public static Action<string> SeenRoberto        = delegate { }; // levelID
+    [Obsolete] public static Action<string> AddLevelComplete   = delegate { }; // levelID
+    [Obsolete] public static Action<string, float> NewBestTime = delegate { }; // levelID time
+    [Obsolete] public static Action<string> BronzeMedalUnlock  = delegate { }; // levelID
+    [Obsolete] public static Action<string> SilverMedalUnlock  = delegate { }; // levelID
+    [Obsolete] public static Action<string> GoldMedalUnlock    = delegate { }; // levelID
 
-    public static Action<string> AddLevelComplete   = delegate { }; // levelID
-    public static Action<string, float> NewBestTime = delegate { }; // levelID time
-    public static Action<string> BronzeMedalUnlock  = delegate { }; // levelID
-    public static Action<string> SilverMedalUnlock  = delegate { }; // levelID
-    public static Action<string> GoldMedalUnlock    = delegate { }; // levelID
+    // For achievements
     public static Action<LevelCompleteData> OnLevelComplete = delegate { };
+    public static Action<string>            SeenRoberto     = delegate { }; // levelID
 
-    public struct LevelCompleteData {
-        public int World;
-    }
+    // For leaderboards
+    public static Action<float>     OnNewBestTime       = delegate { }; // The time
+
+
+    // For multiplayer
+    public static Action<string>    OnPlayerJoinedGame  = delegate { }; // playerName
+    public static Action<string>    OnPlayerLeavedGame  = delegate { }; // playerName
+}
+
+public struct LevelCompleteData
+{
+    public string Level;
+    public int World;
+    public float Time;
 }
