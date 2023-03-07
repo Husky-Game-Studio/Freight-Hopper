d4dacad7113d8243bd3d5f558e85eccba03a58fd
AJ213 <ayrtonmuniz2002@gmail.com>
2023-03-06 23:33:55 -0800

    new jump for realz? Need to tweak

---
+++ Assets/Scripts/Player/Movement/Abilities/JumpBehaviorInputs.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Inputs/JumpBehaviorInputs", fileName = "JumpBehaviorInputs")]
+public class JumpBehaviorInputs : ScriptableObject
+{
+    public float intialVelocity = 2f;
+    public float holdingJumpForceMultiplier = 5f;
+    public float jumpCurveOffset = 1f;
+    public AnimationCurve jumpCurve;
+}
