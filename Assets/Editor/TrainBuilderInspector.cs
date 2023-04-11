743226b164819dc4e02cf87605ecc817ff21771c
Gabriel O <olivergabe@hotmail.com>
2023-04-10 23:40:06 -0700

    updated 1-7

--- Assets/Editor/TrainBuilderInspector.cs
+++ Assets/Editor/TrainBuilderInspector.cs
@@ -96,17 +96,6 @@ public class TrainBuilderInspector : Editor
             trainBuilder.UpdateAllTrains();
         }
         EditorGUILayout.Space(10);
-        if (GUILayout.Button("Move Forward"))
-        {
-            trainBuilder.DecrementActionIndex();
-            trainBuilder.OnValidate();
-        }
-        if (GUILayout.Button("Move Backwards"))
-        {
-            trainBuilder.IncrementActionIndex();
-            trainBuilder.OnValidate();
-        }
-        EditorGUILayout.Space(10);
         if (GUILayout.Button("Swap Forward"))
         {
             trainBuilder.SwapForward();
@@ -115,6 +104,17 @@ public class TrainBuilderInspector : Editor
         if (GUILayout.Button("Swap Backwards"))
         {
             trainBuilder.SwapBackward();
+            trainBuilder.OnValidate();
+        }
+        EditorGUILayout.Space(10);
+        if (GUILayout.Button("Move Forward"))
+        {
+            trainBuilder.DecrementActionIndex();
+            trainBuilder.OnValidate();
+        }
+        if (GUILayout.Button("Move Backwards"))
+        {
+            trainBuilder.IncrementActionIndex();
             trainBuilder.OnValidate();
         }
 
